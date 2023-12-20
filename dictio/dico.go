4457d5090a9ce7b02afa830aa8eb143f395c2e91
Greglhub <76042882+Greglhub@users.noreply.github.com>
2023-12-20 15:07:31 +0100

    Modification dico main

--- dictio/dico.go
+++ dictio/dico.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"sort"
 	"strings"
+	"sync"
 )
 
 type Entry struct {
@@ -14,15 +15,25 @@ type Entry struct {
 
 type Dictionary struct {
 	FilePath string
+	mu       sync.Mutex
+	AddCh    chan Entry
+	RemoveCh chan string
 }
 
 func NewDictionary(filePath string) *Dictionary {
-	return &Dictionary{FilePath: filePath}
+	return &Dictionary{
+		FilePath: filePath,
+		AddCh:    make(chan Entry),
+		RemoveCh: make(chan string),
+	}
 }
 
 func (d *Dictionary) Add(word, definition string) {
-	entry := Entry{Word: word, Definition: definition}
-	d.writeEntryToFile(entry)
+	d.AddCh <- Entry{Word: word, Definition: definition}
+}
+
+func (d *Dictionary) Remove(word string) {
+	d.RemoveCh <- word
 }
 
 func (d *Dictionary) Get(word string) (string, bool) {
@@ -35,15 +46,27 @@ func (d *Dictionary) Get(word string) (string, bool) {
 	return "", false
 }
 
-func (d *Dictionary) Remove(word string) {
-	entries := d.readEntriesFromFile()
-	filteredEntries := make([]Entry, 0, len(entries))
-	for _, entry := range entries {
-		if entry.Word != word {
-			filteredEntries = append(filteredEntries, entry)
-		}
+func (d *Dictionary) handleAdd() {
+	for entry := range d.AddCh {
+		d.mu.Lock()
+		d.writeEntryToFile(entry)
+		d.mu.Unlock()
 	}
-	d.writeEntriesToFile(filteredEntries)
+}
+
+func (d *Dictionary) handleRemove() {
+	for word := range d.RemoveCh {
+		d.mu.Lock()
+		entries := d.readEntriesFromFile()
+		filteredEntries := make([]Entry, 0, len(entries))
+		for _, entry := range entries {
+			if entry.Word != word {
+				filteredEntries = append(filteredEntries, entry)
+			}
+		}
+		d.writeEntriesToFile(filteredEntries)
+		d.mu.Unlock()
+	}
 }
 
 func (d *Dictionary) List() []string {
@@ -58,23 +81,43 @@ func (d *Dictionary) List() []string {
 	return result
 }
 
+func (d *Dictionary) StartWorkers() {
+	go d.handleAdd()
+	go d.handleRemove()
+}
+
 func (d *Dictionary) writeEntryToFile(entry Entry) {
-	file, _ := os.OpenFile(d.FilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	file, err := os.OpenFile(d.FilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
 	defer file.Close()
+
 	fmt.Fprintf(file, "%s|%s\n", entry.Word, entry.Definition)
 }
 
 func (d *Dictionary) writeEntriesToFile(entries []Entry) {
-	file, _ := os.OpenFile(d.FilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	file, err := os.OpenFile(d.FilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
 	defer file.Close()
+
 	for _, entry := range entries {
 		fmt.Fprintf(file, "%s|%s\n", entry.Word, entry.Definition)
 	}
 }
 
 func (d *Dictionary) readEntriesFromFile() []Entry {
-	file, _ := os.Open(d.FilePath)
+	file, err := os.Open(d.FilePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil
+	}
 	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 	var entries []Entry
 	for scanner.Scan() {
--- main.go
+++ main.go
@@ -2,29 +2,57 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"Dictionnaire.go/dictio"
 )
 
 func main() {
 	dictionaryFilePath := "dictionary.json"
-	dict := dictio.NewDictionary(dictionaryFilePath)
+	dico := dictio.NewDictionary(dictionaryFilePath)
 
-	dict.Add("go", "A programming language")
-	dict.Add("map", "A data structure")
+	// Démarrer les workers de gestion concurrente
+	dico.StartWorkers()
 
-	definition, exists := dict.Get("go")
-	if exists {
-		fmt.Printf("Definition of 'go': %s\n", definition)
-	} else {
-		fmt.Println("Word not found in the dictionary")
-	}
+	var wg sync.WaitGroup
 
-	dict.Remove("map")
+	// Exemple d'utilisation concurrente
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		dico.Add("go", "A programming language")
+	}()
 
-	wordList := dict.List()
-	fmt.Println("Dictionary entries:")
-	for _, entry := range wordList {
-		fmt.Println(entry)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		definition, exists := dico.Get("go")
+		if exists {
+			fmt.Printf("Definition of 'go': %s\n", definition)
+		} else {
+			fmt.Println("Word not found in the dictionary")
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		dico.Remove("map")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		wordList := dico.List()
+		fmt.Println("Dictionary entries:")
+		for _, entry := range wordList {
+			fmt.Println(entry)
+		}
+	}()
+
+	wg.Wait()
+
+	// Fermer les channels après que toutes les opérations soient terminées
+	close(dico.AddCh)
+	close(dico.RemoveCh)
 }
